904c9d50b5515f6a33cb0e9adf4a010cfc5c0663
sunny8212 <padariyasunny@gmail.com>
2025-01-27 13:27:12 +0530

    login page updated

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,22 @@
   <div class="h-screen w-screen bg-gray-300 flex items-center justify-center overflow-hidden">
     <div class="bg-white shadow-2xl rounded-4xl p-10 w-full max-w-sm">
       <h2 class="text-2xl font-bold text-center text-gray-800">LOGIN</h2>
-      <form class="mt-8" @submit.prevent="handleLogin">
+      <form class="mt-8">
         <div class="mb-4">
           <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
           <input
-            v-model="email"
             type="email"
             id="email"
-            class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg"
+            class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg "
             placeholder="Enter your email"
           />
         </div>
         <div class="mb-4">
           <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
           <input
-            v-model="password"
             type="password"
             id="password"
-            class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg"
+            class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg  "
             placeholder="Enter your password"
           />
         </div>
@@ -38,35 +36,3 @@
   </div>
 </template>
 
-<script>
-import axios from "axios";
-
-export default {
-  data() {
-    return {
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    async handleLogin() {
-      try {
-        const response = await axios.post("http://localhost:3000/api/auth/login", {
-          email: this.email,
-          password: this.password,
-        });
-
-        // Save the token to localStorage
-        const token = response.data.token;
-        localStorage.setItem("authToken", token);
-
-        // Redirect the user after successful login
-        this.$router.push("/dashboard");
-      } catch (error) {
-        console.error("Login error:", error.response.data.message);
-        alert("Login failed: " + error.response.data.message);
-      }
-    },
-  },
-};
-</script>
